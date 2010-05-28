27a75209c2f5da25f12d4564667a63b12d8c8f58
Nathan Bruning <nathan.bruning@gmail.com>
2010-05-28 11:25:35 +0000

    Fix ant version property provider
    svn path=/webdsl-editor/trunk/; revision=3954

--- webdsl.editor/editor/java/webdsl/WebDSLVersionPropertyProvider.java
+++ webdsl.editor/editor/java/webdsl/WebDSLVersionPropertyProvider.java
@@ -1,18 +1,16 @@
 package webdsl;
 
 import org.eclipse.ant.core.IAntPropertyValueProvider;
-import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 
 public class WebDSLVersionPropertyProvider implements IAntPropertyValueProvider {
 	
 	/**
-	 * Return plugin version in major.minor.micro[.qualifier] format.
+	 * Return plug-in version in major.minor.micro[.qualifier] format.
 	 */
 	public String getAntPropertyValue(String antPropertyName) {
      	
-		// Plugin might not be started, so we can't use Activator.getInstance()
-		Bundle bundle = Platform.getBundle(Activator.kPluginID);
+		Bundle bundle = Activator.getInstance().getBundle();
 		return bundle.getVersion().toString();
 		
 	}
