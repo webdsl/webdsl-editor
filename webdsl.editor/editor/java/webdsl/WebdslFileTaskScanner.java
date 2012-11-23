38b4ec779b3ac44e179a7b3219d15580883dd2a9
Elmer van Chastelet <evanchastelet@gmail.com>
2012-11-23 14:40:54 +0000

    svn path=/webdsl-editor/trunk/; revision=5531

--- webdsl.editor/editor/java/webdsl/WebdslFileTaskScanner.java
+++ webdsl.editor/editor/java/webdsl/WebdslFileTaskScanner.java
@@ -35,116 +35,116 @@ import org.strategoxt.tools.var2prodrule_0_0;
  */
 public class WebdslFileTaskScanner implements IFileTaskScanner {
 
-	private ArrayList<Map> MarkerMapList = null;
-	
-	
-	
-	public WebdslFileTaskScanner() {
-		super();
-		MarkerMapList = new ArrayList<Map>();
-	}
-	
-	private void setMarkerOnFile(IFile file, int lineNumber, String text, int priority, int start, int end){
+    private ArrayList<Map> MarkerMapList = null;
+    
+    
+    
+    public WebdslFileTaskScanner() {
+        super();
+        MarkerMapList = new ArrayList<Map>();
+    }
+    
+    private void setMarkerOnFile(IFile file, int lineNumber, String text, int priority, int start, int end){
 
-		try {
-			IMarker newmarker = file.createMarker(getMarkerType());
-			newmarker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
-			newmarker.setAttribute(IMarker.MESSAGE, text);
-			newmarker.setAttribute(IMarker.PRIORITY, priority);
-			newmarker.setAttribute(IMarker.CHAR_START,start);
-			newmarker.setAttribute(IMarker.CHAR_END, end);
-			newmarker.setAttribute(IMarker.USER_EDITABLE, false);
+        try {
+            IMarker newmarker = file.createMarker(getMarkerType());
+            newmarker.setAttribute(IMarker.LINE_NUMBER,lineNumber);
+            newmarker.setAttribute(IMarker.MESSAGE, text);
+            newmarker.setAttribute(IMarker.PRIORITY, priority);
+            newmarker.setAttribute(IMarker.CHAR_START,start);
+            newmarker.setAttribute(IMarker.CHAR_END, end);
+            newmarker.setAttribute(IMarker.USER_EDITABLE, false);
 
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public String getMarkerType() {
-		
-		return "org.eclipse.core.resources.taskmarker";
-	}
+    @Override
+    public String getMarkerType() {
+        
+        return "org.eclipse.core.resources.taskmarker";
+    }
 
-	@Override
-	public Map[] scan(IFile file, TaskTag[] tags, IProgressMonitor arg2) {
-		try {
-			file.deleteMarkers(null, true, 1);
-		} catch (CoreException e1) {
-			e1.printStackTrace();
-		}
-		if(!arg2.isCanceled() ){
-			Scanner scanner = null;
-			try {
-				scanner = new Scanner(file.getContents());
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}	
-			int lineNumber = 0;
-			int offset = 0;
-				while(scanner.hasNextLine()){
-					String line= "";
-					try{
-						lineNumber ++;
-						line = scanner.nextLine();
-						if (line.contains("//")){
-							String[] split = line.split("//");
-							if(split.length > 1){
-								String comment = line.split("//")[1];
-								if(comment.contains(":")) {
-									split = comment.split(":");
-									if(split.length > 1){
-										String tag = split[0].trim();
-										String description = split[1];
-										if(stringIsTagName(tag, tags)){
-											int start = offset + line.indexOf(tag);
-											int end = offset + line.replaceAll("\\s+$", "").length();
-											setMarkerOnFile(file, lineNumber, tag + " : "+ description.trim(), findPriorityOfTag(tag, tags), start, end);
-										}
-									}
-								}							
-							}
-						}
-					}catch (Exception e) {
-						e.printStackTrace();
-					} finally {
-						offset += line.length() + 1;// + 1 is end of line char
-					}
-				}
-				
-		}
-		
-		return (Map[]) MarkerMapList.toArray(new Map[MarkerMapList.size()]);
-	} 
+    @Override
+    public Map[] scan(IFile file, TaskTag[] tags, IProgressMonitor arg2) {
+        try {
+            file.deleteMarkers(null, true, 1);
+        } catch (CoreException e1) {
+            e1.printStackTrace();
+        }
+        if(!arg2.isCanceled() ){
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(file.getContents());
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }    
+            int lineNumber = 0;
+            int offset = 0;
+                while(scanner.hasNextLine()){
+                    String line= "";
+                    try{
+                        lineNumber ++;
+                        line = scanner.nextLine();
+//                        if (line.contains("//")){
+                            String[] split = line.split("//");
+                            if(split.length > 1){
+                                String comment = split[1];
+//                                if(comment.contains(":")) {
+                                    split = comment.split(":");
+                                    if(split.length > 1){
+                                        String tag = split[0].trim();
+                                        String description = split[1];
+                                        if(stringIsTagName(tag, tags)){
+                                            int start = offset + line.indexOf(tag);
+                                            int end = offset + line.replaceAll("\\s+$", "").length();
+                                            setMarkerOnFile(file, lineNumber, tag + " : "+ description.trim(), findPriorityOfTag(tag, tags), start, end);
+                                        }
+                                    }
+//                                }                            
+//                            }
+                        }
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        offset += line.length() + 1;// + 1 is end of line char
+                    }
+                }
+                
+        }
+        
+        return (Map[]) MarkerMapList.toArray(new Map[MarkerMapList.size()]);
+    } 
 
 
 
-	private int findPriorityOfTag(String tag, TaskTag[] tags) {
-		for (TaskTag taskTag : tags) {
-			if(taskTag.getTag().equalsIgnoreCase(tag)){
-				return taskTag.getPriority();
-			}
-		}
-		return -1;
-	}
+    private int findPriorityOfTag(String tag, TaskTag[] tags) {
+        for (TaskTag taskTag : tags) {
+            if(taskTag.getTag().equalsIgnoreCase(tag)){
+                return taskTag.getPriority();
+            }
+        }
+        return -1;
+    }
 
-	private boolean stringIsTagName(String term, TaskTag[] tags) {
-		for(TaskTag tag : tags ){
-			if(tag.getTag().equalsIgnoreCase(term)){
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean stringIsTagName(String term, TaskTag[] tags) {
+        for(TaskTag tag : tags ){
+            if(tag.getTag().equalsIgnoreCase(term)){
+                return true;
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public void shutdown(IProject arg0) {
-		
-	}
+    @Override
+    public void shutdown(IProject arg0) {
+        
+    }
 
-	@Override
-	public void startup(IProject arg0) {
+    @Override
+    public void startup(IProject arg0) {
 
-	}
-	
+    }
+    
 }
